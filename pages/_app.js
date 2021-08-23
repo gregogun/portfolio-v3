65904fb32df92e6b3cfc8bd17090b69afec3544a
Greg Ogun <gregogun97@gmail.com>
2021-08-23 09:43:58 +0100

    change default theme to dark

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,16 @@
-import { darkTheme } from '../stitches.config'
-import { ThemeProvider } from 'next-themes'
+import { darkTheme } from '../stitches.config';
+import { ThemeProvider } from 'next-themes';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider attribute="class"
-    defaultTheme="system" value={{ dark: darkTheme.className, light: 'light', }}>
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="dark"
+      value={{ dark: darkTheme.className, light: 'light' }}
+    >
       <Component {...pageProps} />
     </ThemeProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
