566f80e6d56e962cfee094e918ecdbbb6e02db94
Greg Ogun <gregogun97@gmail.com>
2021-08-24 18:44:50 +0100

    add social icons

---
+++ components/icons/twitch.tsx
@@ -0,0 +1,15 @@
+const Twitch = ({ ...props }) => {
+  return (
+    <svg
+      {...props}
+      fill="var(--colors-secondary)"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+    >
+      <path d="M3.75 1.5l-1.5 3.75V19.5h4.5v3h3l3-3h3.75l5.25-5.25V1.5h-18zm15.75 12l-3 3H12l-3 3v-3H5.25V3.75H19.5v9.75z" />
+      <path d="M15 6.703h2.25v6.047H15V6.703zm-5.25 0H12v6.047H9.75V6.703z" />
+    </svg>
+  );
+};
+
+export default Twitch;
