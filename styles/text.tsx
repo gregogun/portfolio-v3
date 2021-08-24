aa1823fb77a3d64050c5b8172af0acfbeae6aaf5
Greg Ogun <gregogun97@gmail.com>
2021-08-24 06:58:28 +0100

    add text variants

--- styles/text.tsx
+++ styles/text.tsx
@@ -1,9 +1,68 @@
 import { css } from '../stitches.config';
 
 export const text = css({
+  boxSizing: 'border-box',
   fontFamily: '$sans',
   lineHeight: 'inherit',
   margin: 0,
-  fontSize: '18px',
-  fontWeight: '400'
+
+  variants: {
+    size: {
+      xs: {
+        fontSize: '14px',
+        lineHeight: '21px'
+      },
+      sm: {
+        fontSize: '16px',
+        lineHeight: '22px'
+      },
+      md: {
+        fontSize: '18px',
+        lineHeight: '24px'
+      },
+      lg: {
+        fontSize: '20px',
+        lineHeight: '30px'
+      },
+      xl: {
+        fontSize: '24px',
+        lineHeight: '36px'
+      },
+      '2xl': {
+        fontSize: '32px',
+        lineHeight: '48px'
+      },
+      '3xl': {
+        fontSize: '40px',
+        lineHeight: '48px'
+      },
+      '4xl': {
+        fontSize: '48px',
+        lineHeight: '48px'
+      },
+      title: {
+        fontSize: '64px',
+        lineHeight: '72px'
+      }
+    },
+    weight: {
+      bold: {
+        fontStyle: 'normal',
+        fontWeight: '600'
+      },
+      semibold: {
+        fontStyle: 'normal',
+        fontWeight: '500'
+      },
+      regular: {
+        fontStyle: 'normal',
+        fontWeight: '400'
+      }
+    }
+  },
+
+  defaultVariants: {
+    size: 'md',
+    weight: 'regular'
+  }
 });
