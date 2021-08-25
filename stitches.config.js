9a20670687d7b7ecff692bbefca90d8cbf706798
Greg Ogun <gregogun97@gmail.com>
2021-08-25 21:52:30 +0100

    chore: add local fonts and unicode ranges

--- stitches.config.js
+++ stitches.config.js
@@ -89,19 +89,18 @@ export const globalStyles = stitchesConfig.global({
   '@font-face': {
     fontFamily: 'Inter',
     // fontStyle: 'normal',
-    fontWeight: '400 600 700',
+    fontWeight: '400 600 ',
     fontDisplay: 'optional',
-    src: 'url(./public/fonts/inter-var-latin.woff2) format("woff2")',
-    unicodeRange: `U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA,
-    U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215,
-    U+FEFF, U+FFFD`
+    src: 'local("Inter") url(/fonts/inter-var-latin.woff2) format("woff2")',
+    unicodeRange: `U+000-5FF`
   },
   '@font-face': {
     fontFamily: 'Poppins',
     fontStyle: 'normal',
     fontWeight: 600,
     fontDisplay: 'optional',
-    src: 'url(./public/fonts/poppins-v15-latin-600.woff) format("woff") url(./public/fonts/poppins-v15-latin-600.woff2) format("woff2")'
+    src: 'local("Poppins") url(/fonts/poppins-v15-latin-600.woff) format("woff") url(/fonts/poppins-v15-latin-600.woff2) format("woff2")',
+    unicodeRange: `U+000-5FF`
   }
 });
 
