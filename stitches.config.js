6a67a8e69dc631be3e09815b923584bbf73ba02c
Greg Ogun <gregogun97@gmail.com>
2021-08-25 13:53:45 +0100

    chore: replace existing fonts with variable fonts

--- stitches.config.js
+++ stitches.config.js
@@ -81,27 +81,16 @@ export const globalStyles = stitchesConfig.global({
   '@font-face': {
     fontFamily: 'Inter',
     // fontStyle: 'normal',
-    fontWeight: 400,
+    fontWeight: '400 600 700',
     fontDisplay: 'optional',
-    src: 'url(./public/fonts/inter-v3-latin-regular.woff) format("woff") url(./public/fonts/inter-v3-latin-regular.woff2) format("woff2")'
-  },
-  '@font-face': {
-    fontFamily: 'Inter',
-    // fontStyle: 'normal',
-    fontWeight: 600,
-    fontDisplay: 'optional',
-    src: 'url(./public/fonts/inter-v3-latin-600.woff) format("woff") url(./public/fonts/inter-v3-latin-600.woff2) format("woff2")'
-  },
-  '@font-face': {
-    fontFamily: 'Inter',
-    // fontStyle: 'normal',
-    fontWeight: 800,
-    fontDisplay: 'optional',
-    src: 'url(./public/fonts/inter-v3-latin-800.woff) format("woff") url(./public/fonts/inter-v3-latin-800.woff2) format("woff2")'
+    src: 'url(./public/fonts/inter-var-latin.woff2) format("woff2")',
+    unicodeRange: `U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA,
+    U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215,
+    U+FEFF, U+FFFD`
   },
   '@font-face': {
     fontFamily: 'Poppins',
-    // fontStyle: 'normal',
+    fontStyle: 'normal',
     fontWeight: 600,
     fontDisplay: 'optional',
     src: 'url(./public/fonts/poppins-v15-latin-600.woff) format("woff") url(./public/fonts/poppins-v15-latin-600.woff2) format("woff2")'
