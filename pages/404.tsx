4e83dcf74b2d5ec627fcf80cf443ce44b3d394d6
Greg Ogun <gregogun97@gmail.com>
2021-08-27 09:19:55 +0100

    feat: add 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,21 @@
+import { Container } from 'pages';
+import Link from 'next/link';
+import { link } from '@/styles/link';
+
+export default function NotFound() {
+  return (
+    <Container
+      css={{
+        display: 'grid',
+        placeItems: 'center',
+        textAlign: 'center',
+        paddingTop: '$7'
+      }}
+    >
+      <h1>It seems you've lost your way traveller. Let us guide you home.</h1>
+      <Link href="/">
+        <a className={link()}>Take me home, website gods.</a>
+      </Link>
+    </Container>
+  );
+}
