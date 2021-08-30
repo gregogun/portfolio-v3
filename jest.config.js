c7ecba9dac885600c0d6f1fd5c8f3f37ed00b730
Greg Ogun <gregogun97@gmail.com>
2021-08-30 07:52:58 +0100

    chore: install and setup jest and rtl

---
+++ jest.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  moduleNameMapper: {
+    '^@/components/(.*)$': '<rootDir>/components/$1',
+    '^@/utils/(.*)$': '<rootDir>/utils/$1',
+    '^@/data/(.*)$': '<rootDir>/data/$1',
+    '^@/styles/(.*)$': '<rootDir>/styles/$1',
+    '^@/pages/(.*)$': '<rootDir>/pages/$1'
+  },
+  testPathIgnorePatterns: [
+    '<rootDir>/.next/',
+    '<rootDir>/node_modules/',
+    '<rootDir>/coverage',
+    '<rootDir>/dist'
+  ],
+  moduleDirectories: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/src',
+    '<rootDir>/pages'
+  ],
+  testURL: `http://localhost`,
+  setupFilesAfterEnv: ['<rootDir>/test/setup.ts'],
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    'pages/**/*.{js,jsx,ts,tsx}'
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 0,
+      functions: 0,
+      lines: 0,
+      statements: 0
+    }
+  }
+};
