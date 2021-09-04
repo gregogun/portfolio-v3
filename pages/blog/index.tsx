997e0bca2ff3028adf659fc455491655a5142c93
gregogun <gregogun97@gmail.com>
2021-09-04 12:58:25 +0100

    style: add styling to blog index page

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,6 +1,7 @@
 import NextLink from 'next/link';
 import { getAllPosts } from '@/lib/mdx';
 import Container from '@/components/container';
+import { link } from '@/styles/link';
 
 export default function Posts({ posts }) {
   return (
@@ -8,8 +9,16 @@ export default function Posts({ posts }) {
       <ul>
         {posts.map((post, index) => (
           <li key={index}>
-            <NextLink href={`blog/${post.slug}`}>
-              {post.frontmatter.title}
+            <NextLink href={`/blog/${post.slug}`}>
+              <a
+                className={link({
+                  css: {
+                    fontSize: '32px'
+                  }
+                })}
+              >
+                {post.frontmatter.title}
+              </a>
             </NextLink>
           </li>
         ))}
