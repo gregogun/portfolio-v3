57bb130a5b3b408ce51825540333ff28698324f6
Greg Ogun <gregogun97@gmail.com>
2021-09-05 13:33:24 +0100

    style: add variants to link style

--- styles/link.tsx
+++ styles/link.tsx
@@ -1,15 +1,43 @@
 import { css } from '../stitches.config';
 
 export const link = css({
-  WebkitTapHighlightColor: 'rgba(0, 0, 0, 0)',
-  color: 'inherit',
-  boxShadow: '0px 1px 0px 0px $colors$tertiary',
-  transition: 'all 200ms linear 0ms',
-  fontWeight: 'bold',
-  textDecoration: 'none',
+  variants: {
+    type: {
+      line: {
+        WebkitTapHighlightColor: 'rgba(0, 0, 0, 0)',
+        color: 'inherit',
+        boxShadow: '0px 1px 0px 0px $colors$tertiary',
+        transition: 'all 200ms linear 0ms',
+        fontWeight: 'bold',
+        textDecoration: 'none',
 
-  '&:hover': {
-    color: '$tertiary',
-    transition: 'all 200ms linear 0ms'
+        '&:hover': {
+          color: '$tertiary',
+          transition: 'all 200ms linear 0ms'
+        }
+      },
+      noLine: {
+        WebkitTapHighlightColor: 'rgba(0, 0, 0, 0)',
+        color: 'inherit',
+        transition: 'all 200ms linear 0ms',
+        fontWeight: 'bold',
+        textDecoration: 'none',
+
+        '&:hover': {
+          color: '$tertiary',
+          transition: 'all 200ms linear 0ms'
+        }
+      },
+      ghost: {
+        WebkitTapHighlightColor: 'rgba(0, 0, 0, 0)',
+        color: 'inherit',
+        transition: 'all 200ms linear 0ms',
+        fontWeight: 'bold',
+        textDecoration: 'none'
+      }
+    }
+  },
+  defaultVariants: {
+    type: 'line'
   }
 });
