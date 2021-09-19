19f5d91c4f2cba2c001a60dc533c8962279f532e
Greg Ogun <gregogun97@gmail.com>
2021-09-19 17:50:32 +0100

    chore: add data for xylo project

---
+++ data/projects/xylo.tsx
@@ -0,0 +1,6 @@
+export const xylo = {
+  title: 'Xylo',
+  src: 'https://gregogun-xylo.vercel.app/',
+  summary: 'A finance dashboard powered by the Plaid Open Banking API.',
+  imgSrc: '/static/images/xylo/homepage.jpg',
+};
