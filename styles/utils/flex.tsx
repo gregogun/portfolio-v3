0fade4cac17aedc24bf5dc5a236ba416ce91ba3a
Greg Ogun <gregogun97@gmail.com>
2021-09-24 19:47:44 +0100

    style: enhance flex style object

--- styles/utils/flex.tsx
+++ styles/utils/flex.tsx
@@ -2,5 +2,43 @@ import { css } from 'stitches.config';
 
 export const flex = css({
   display: 'flex',
-  alignItems: 'center',
+
+  variants: {
+    flow: {
+      horizontal: {
+        flexDirection: 'row',
+      },
+      vertical: {
+        flexDirection: 'column',
+      },
+    },
+    spaced: {
+      none: {
+        justifyContent: 'normal',
+      },
+      even: {
+        justifyContent: 'space-evenly',
+      },
+      full: {
+        justifyContent: 'space-between',
+      },
+    },
+    alignment: {
+      start: {
+        alignItems: 'start',
+      },
+      center: {
+        alignItems: 'center',
+      },
+      end: {
+        alignItems: 'end',
+      },
+    },
+  },
+
+  defaultVariants: {
+    flow: 'horizontal',
+    spaced: 'none',
+    alignment: 'center',
+  },
 });
